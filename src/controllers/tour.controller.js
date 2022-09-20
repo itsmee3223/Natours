8de51c8cb09f4c9ab62a5c2fd0cdc29de684de91
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-20 21:02:48 +0700

    /PATCH Update tour images

--- src/controllers/tour.controller.js
+++ src/controllers/tour.controller.js
@@ -3,3 +3,5 @@ const TourSchema = require("../models/Tour.schema");
 
 exports.httpGetAllTours = handlerFactory.getAll(TourSchema);
 exports.httpGetTour = handlerFactory.getOne(TourSchema, { path: "reviews" });
+exports.httpCreateTour = handlerFactory.createOne(TourSchema);
+exports.httpUpdateTour = handlerFactory.updateOne(TourSchema);
--- src/routes/tour.routes.js
+++ src/routes/tour.routes.js
@@ -3,10 +3,19 @@ const router = require("express").Router();
 const {
   httpGetAllTours,
   httpGetTour,
+  httpCreateTour,
+  httpUpdateTour,
 } = require("../controllers/tour.controller");
+const { uploadTourImages, resizeTourImages } = require("../middleware/uploadImage");
 
-router.route("/").get(httpGetAllTours);
 
-router.route("/:id").get(httpGetTour);
+router.route("/")
+  .get(httpGetAllTours)
+  .post(httpCreateTour);
+
+router
+  .route("/:id")
+  .get(httpGetTour)
+  .patch(uploadTourImages, resizeTourImages, httpUpdateTour);
 
 module.exports = router;
