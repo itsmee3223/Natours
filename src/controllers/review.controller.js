26f1accb2f599aef295366fa15520b74f6bdbda2
itsmee3223 <tamvam.pemberani@gmail.com>
2022-09-24 03:59:06 +0700

    /POST create review

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,7 +1,7 @@
 const handlerFactory = require("../models/handlerFactory");
 const ReviewSchema = require("../models/Review.schema");
 
-exports.setTourId = (req, res, next) => {
+exports.setTourIdAndUserId = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.setTourId;
   if (!req.body.user) req.body.user = req.user.id;
   next();
@@ -9,3 +9,4 @@ exports.setTourId = (req, res, next) => {
 
 exports.httpGetAllReviews = handlerFactory.getAll(ReviewSchema);
 exports.httpGetReview = handlerFactory.getOne(ReviewSchema);
+exports.httpCreateReview = handlerFactory.createOne(ReviewSchema);
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
 
-const { httpGetAllReviews, httpGetReview } = require("../controllers/review.controller");
+const {
+  httpGetAllReviews,
+  httpGetReview,
+  setTourIdAndUserId,
+  httpCreateReview,
+} = require("../controllers/review.controller");
 const userMiddleware = require("../middleware/user.middleware");
 router.use(userMiddleware.auth);
 
-router.route("/").get(httpGetAllReviews);
+router
+  .route("/")
+  .get(httpGetAllReviews)
+  .post(userMiddleware.authorize("user"), setTourIdAndUserId, httpCreateReview);
+
 router.route("/:id").get(httpGetReview);
+
 module.exports = router;
